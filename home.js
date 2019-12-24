70e9f13b724ad5c674b005b59900961a63714ba7
Novah <nour@Nours-MacBook-Pro.local>
2019-12-24 08:55:08 -0800

    Calculates default questions answer

--- home.js
+++ home.js
@@ -10,11 +10,11 @@ function addAge(){
 
 function calculateChangeInX(){
    let t = solve(-2.45,4,5);
-   console.log(Math.round(6.9*t,2) + ' m');
+   document.getElementById("solution").innerHTML = Math.round(6.9*t,2) + ' m';
 }
 
 function solve(a, b, c) {
     var result = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
     var result2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
-    return Math.max(result,result2);
+    return Math.max(result,result2);xw
 }
