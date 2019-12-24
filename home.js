062ba40f0936cc81d50599a3597b9f471957c403
Novah <nour@Nours-MacBook-Pro.local>
2019-12-23 21:08:25 -0800

    Added Given

--- home.js
+++ home.js
@@ -3,12 +3,18 @@ console.log('hello');
 
 
 function addAge(){
-    var inputVal = document.getElementById('fName');
+    var inputVal = document.getElementById('InV');
     console.log('My age is : ' + inputVal);
 }
 
 
 function calculateChangeInX(){
-    
+   let t = solve(-2.45,4,5);
+   console.log(Math.round(6.9*t,2) + ' m');
+}
 
+function solve(a, b, c) {
+    var result = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+    var result2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+    return Math.max(result,result2);
 }
