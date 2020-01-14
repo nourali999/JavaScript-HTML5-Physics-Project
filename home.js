a7b28c40d39012c7f5c0c3f98a635b471a74fed0
nourali999 <noahali460@gmail.com>
2020-01-13 23:05:46 -0800

    input field works dynamically

--- home.js
+++ home.js
@@ -1,22 +1,13 @@
 var output = document.getElementById("container-output");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.log('hello');
-
-
-
+var initialVelocity;
+var height;
+var angle;
+var a;
+var b;
+var c;
+var t;
+var Vox;
+var Voy = b;
 
 
 
@@ -29,12 +20,33 @@ console.log('hello');
 
 
 function calculateChangeInX(){
-   let t = solve(-2.45,4,5);
-   var initialVelocity = document.getElementById("InV").value;
-   var height = document.getElementById("h").value;
-   var angle = document.getElementById("angl").value;
-   document.getElementById("solution").innerHTML = Math.round(6.9*t,2) + ' m ' + initialVelocity;
-   
+    initialVelocity = document.getElementById("InV").value;
+    height = document.getElementById("h").value;
+    angle = document.getElementById("angl").value;
+
+    if(isNaN(initialVelocity+height+angle)){
+
+        document.getElementById("solution").innerHTML = 'Please enter a valid number'; 
+
+    }else {
+
+    a = -2.45;
+    
+    // b is equavalent to Voy 
+    b = (initialVelocity*Math.sin(angle * Math.PI / 180));
+    
+
+    c = height;
+
+
+    t = solve(a,b,c);
+ 
+
+    Vox = (initialVelocity*Math.cos(angle * Math.PI /180));
+    console.log(a, b, c, Vox);
+
+    document.getElementById("solution").innerHTML = Math.round(Vox*t,2) + ' m';
+    }
 }
 
 
@@ -43,7 +55,7 @@ function calculateChangeInX(){
 
 
 
-function solve(a, b, c) {
+function solve(a , b, c) {
     var result = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
     var result2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
     return Math.max(result,result2);
