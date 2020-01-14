250dae8ebff8c47963a498061473887da82623d9
nourali999 <noahali460@gmail.com>
2020-01-13 17:53:46 -0800

    Added variable from input field

--- home.js
+++ home.js
@@ -1,17 +1,47 @@
+var output = document.getElementById("container-output");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 console.log('hello');
 
 
 
-function addAge(){
-    var inputVal = document.getElementById('InV');
-    console.log('My age is : ' + inputVal);
-}
+
+
+
+
+
+
+
+
+
 
 
 function calculateChangeInX(){
    let t = solve(-2.45,4,5);
-   document.getElementById("solution").innerHTML = Math.round(6.9*t,2) + ' m';
+   var initialVelocity = document.getElementById("InV").value;
+   var height = document.getElementById("h").value;
+   var angle = document.getElementById("angl").value;
+   document.getElementById("solution").innerHTML = Math.round(6.9*t,2) + ' m ' + initialVelocity;
+   
 }
+
+
+
+
+
+
 
 function solve(a, b, c) {
     var result = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
